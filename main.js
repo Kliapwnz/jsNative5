4c5c7d5fc871adb9187b119fb5cf734d13e69853
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-05 19:21:54 +0300

    sort

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ console.log(strings.sort())
 //1. Сортирует строки "из коробки", т.е. без доп. параметров
 const strings2 = ["Donald", "Alex", "Bob", "Дима", "genius", "Натали"]
 console.log(strings2.sort())
+//2. Сортирует в порядке Юникода
