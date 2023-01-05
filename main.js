248c5277b6beeb9f9a2ee68bb3d84c00f9340505
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-05 19:20:27 +0300

    sort

---
+++ main.js
@@ -0,0 +1,5 @@
+const strings = ["Donald", "Alex", "Bob"]
+console.log(strings.sort())
+//1. Сортирует строки "из коробки", т.е. без доп. параметров
+const strings2 = ["Donald", "Alex", "Bob", "Дима", "genius", "Натали"]
+console.log(strings2.sort())
